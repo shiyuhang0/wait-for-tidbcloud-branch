d6702f864a4428cf6bcbd592cc65f182171f5750
shiyuhang <1136742008@qq.com>
2023-03-28 17:47:51 +0800

    test1

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,4 @@
 import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
